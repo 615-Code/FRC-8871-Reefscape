46b65711ce14c90918ba66cd0394c823f59cc5b0
615-Code <Kalebdk10@gmail.com>
2025-02-10 14:44:11 -1000

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
 
-  private spark
+  private Spark Pivotmotor = new Spark(1) 
   
   private Command m_autonomousCommand;
 
